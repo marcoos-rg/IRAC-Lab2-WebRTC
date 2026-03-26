a107c1581495c5a307787f36da13e756ede2638a
marcoos-rg <rosadogonzalezmarcos2@gmail.com>
2026-03-26 10:55:45 +0100

    Final fix?

--- optional/js/completeNodeClientWithDataChannel.js
+++ optional/js/completeNodeClientWithDataChannel.js
@@ -94,7 +94,25 @@ var pc_config = webrtcDetectedBrowser === 'firefox' ?
 }; */
 
 var pc_config = {
-	'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]
+  'iceServers': [
+    { 'urls': 'stun:stun.l.google.com:19302' },
+    { 'urls': 'stun:stun1.l.google.com:19302' },
+    {
+      'urls': 'turn:openrelay.metered.ca:80',
+      'username': 'openrelayproject',
+      'credential': 'openrelayproject'
+    },
+    {
+      'urls': 'turn:openrelay.metered.ca:443',
+      'username': 'openrelayproject',
+      'credential': 'openrelayproject'
+    },
+    {
+      'urls': 'turn:openrelay.metered.ca:443?transport=tcp',
+      'username': 'openrelayproject',
+      'credential': 'openrelayproject'
+    }
+  ]
 };
 
 var pc_constraints = {
